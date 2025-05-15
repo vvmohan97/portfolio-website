0a2a505ddd21e9cec2ea372fdb50eb028057bcdb
vvmohan@97 <vvmohan.vsr@gmail.com>
2025-05-15 16:50:44 +0530

    commit updated

--- src/routing/AppRouter.js
+++ src/routing/AppRouter.js
@@ -1,6 +1,6 @@
 // 
 // /src/routing/AppRouter.js
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ScrollUpButton from "../components/ScrollUpButton";
@@ -14,22 +14,49 @@ const Portfolio = lazy(() => import("../pages/Portfolio"));
 const Contact = lazy(() => import("../pages/Contact"));
 
 const AppRouter = () => {
-  return (
-    <>    
-    
-      <Suspense fallback={<InitialLoader/>}>
+  const [showLoader, setShowLoader] = useState(true);
 
-      <Navbar />
+  useEffect(() => {
+    // Set a timeout to hide the loader after 20 seconds
+    const timer = setTimeout(() => {
+      setShowLoader(false);
+    }, 1000); // 20 seconds
+
+    // Cleanup timer if the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <>
+    {showLoader ? (
+      <InitialLoader />
+    ) : (
+      <Suspense fallback={<InitialLoader />}>
+        <Navbar />
         <Home />
         <Skills />
         <About />
         <Portfolio />
         <Contact />
-    
-      <Footer />
-      <ScrollUpButton />
+        <Footer />
+        <ScrollUpButton />
       </Suspense>
-    </>
+    )}
+  </>
+    // <>    
+    
+    //   <Suspense fallback={<InitialLoader/>}>
+
+    //   <Navbar />
+    //     <Home />
+    //     <Skills />
+    //     <About />
+    //     <Portfolio />
+    //     <Contact />
+    
+    //   <Footer />
+    //   <ScrollUpButton />
+    //   </Suspense>
+    // </>
   );
 };
 
