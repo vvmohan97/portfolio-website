93f0abe390fc907a8a755df666cd88ee19d05f98
MOHAN KUMAR V V <109420343+vvmohan97@users.noreply.github.com>
2025-06-22 11:17:56 +0530

    bot added

---
+++ src/pages/Chatbot.js
@@ -0,0 +1,159 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./chatbot.css";
+import chatIcon from "../asserts/chat_icon.png";
+import closeBotIcon from "../asserts/close_bot.png";
+import AiAgent from "../asserts/ai_agent.png";
+import sendIcon from "../asserts/send_icon.png";
+import { io, Socket } from "socket.io-client";
+const Chatbot = () => {
+  const [isChatbotOpen, setIsChatbotOpen] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [userMsg, setUserMsg] = useState("");
+  const bottomRef = useRef(null);
+  const handleChatbot = () => {
+    setIsChatbotOpen(!isChatbotOpen);
+  };
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setUserMsg(value);
+  };
+
+  const handleSend = () => {
+    if (userMsg.trim() === "") return;
+
+    setLoaderMsg(true);
+
+    setMessages((previousMsg) => [
+      ...previousMsg,
+      { sender: "user", message: userMsg, type: "text" },
+    ]);
+    if (socket && socket.connected) {
+      socket.emit("userMessage", userMsg);
+    } else {
+      console.warn(" Socket not connected. Message not sent.");
+    }
+    setUserMsg("");
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSend();
+    }
+  };
+
+  let socketUrl = `https://chat-bot-web-socket-backend.onrender.com`;
+  const [socket, setSocket] = useState("");
+  const [loaderMsg, setLoaderMsg] = useState(false);
+  useEffect(()=>{
+if(isChatbotOpen){
+       const newSocket = io(socketUrl, {
+        transports: ["websocket"],
+      });
+      setSocket(newSocket);
+         newSocket.emit('userMessage',()=>{
+        setMessages((prev)=>[
+          ...prev,{sender:"user",message:'hi',type:'text'}
+        ])
+      })
+}
+  },[])
+  useEffect(() => {
+    if (isChatbotOpen) {
+      const newSocket = io(socketUrl, {
+        transports: ["websocket"],
+      });
+      setSocket(newSocket);
+      setLoaderMsg(false);
+      newSocket.emit('userMessage',()=>{
+        setMessages((prev)=>[
+          ...prev,{sender:"user",message:'hi',type:'text'}
+        ])
+      })
+      newSocket.on("botReply", (data) => {
+        console.log(data);
+
+        setMessages((prev) => [
+          ...prev,
+          {
+            sender: "bot",
+            message: data,
+            type: "text",
+          },
+        ]);
+      });
+
+      newSocket.on("connect", () => {
+        console.log("socket Connected");
+      });
+      // newSocket.on(`disconnect`,()=>{
+      //   console.log('socket disoonnected');
+
+      // })
+
+      // return ()=>{
+      //   newSocket.disconnect()
+      // }
+    }
+  }, [messages]);
+
+  console.log(messages);
+
+  return (
+    <>
+      <div className="chatbot-container" onClick={handleChatbot}>
+        <img
+          src={isChatbotOpen ? closeBotIcon : chatIcon}
+          alt="chat_icon"
+          className="chat_icon_open"
+        />
+      </div>
+      {isChatbotOpen && (
+        <div className="chat_bot_main_container">
+          <div className="chat-header">
+            <div>
+              <img src={AiAgent} className="ai_agent_header" alt="ai agent" />
+            </div>
+            <div>
+              <h3>AI agent </h3>
+              <p>i will help you to know more </p>
+            </div>
+            <div>
+              <img
+                src={closeBotIcon}
+                onClick={handleChatbot}
+                alt="chat_icon"
+                className="chat_icon_open_header"
+              />
+            </div>
+          </div>
+          <div className="chat-body">
+            {loaderMsg && <div className={`bot-container bot`}>Loading...</div>}
+            {messages?.map((msg, index) => (
+              <>
+                <div key={index} className={`bot-container ${msg.sender}`}>
+                  {msg.message}
+                </div>
+              </>
+            ))}
+          </div>
+          <div ref={bottomRef}></div>
+
+          <div className="chat-footer">
+            <textarea
+              value={userMsg}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+            <img  onClick={handleSend} src={sendIcon} alt="send" />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Chatbot;
--- src/routing/AppRouter.js
+++ src/routing/AppRouter.js
@@ -1,11 +1,12 @@
 // 
 // /src/routing/AppRouter.js
-import React, { Suspense, lazy, useEffect, useState } from "react";
+import React, { Suspense, lazy } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ScrollUpButton from "../components/ScrollUpButton";
 import InitialLoader from "../utils/loader/InitialLoader";
-
+import '../pages/defaultStyles.css';
+import Chatbot from "../pages/Chatbot";
 // Lazy load pages
 const Home = lazy(() => import("../pages/Home"));
 const About = lazy(() => import("../pages/About"));
@@ -14,49 +15,25 @@ const Portfolio = lazy(() => import("../pages/Portfolio"));
 const Contact = lazy(() => import("../pages/Contact"));
 
 const AppRouter = () => {
-  const [showLoader, setShowLoader] = useState(true);
-
-  useEffect(() => {
-    // Set a timeout to hide the loader after 20 seconds
-    const timer = setTimeout(() => {
-      setShowLoader(false);
-    }, 1000); // 20 seconds
-
-    // Cleanup timer if the component unmounts
-    return () => clearTimeout(timer);
-  }, []);
   return (
-    <>
-    {showLoader ? (
-      <InitialLoader />
-    ) : (
-      <Suspense fallback={<InitialLoader />}>
-        <Navbar />
+    <>    
+    
+      <Suspense fallback={<InitialLoader/>}>
+<div className="app-container">
+   <Navbar />
+   <Chatbot />
         <Home />
         <Skills />
         <About />
         <Portfolio />
         <Contact />
-        <Footer />
-        <ScrollUpButton />
+    
+      <Footer />
+      <ScrollUpButton />
+</div>
+   
       </Suspense>
-    )}
-  </>
-    // <>    
-    
-    //   <Suspense fallback={<InitialLoader/>}>
-
-    //   <Navbar />
-    //     <Home />
-    //     <Skills />
-    //     <About />
-    //     <Portfolio />
-    //     <Contact />
-    
-    //   <Footer />
-    //   <ScrollUpButton />
-    //   </Suspense>
-    // </>
+    </>
   );
 };
 
